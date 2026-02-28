68ba7e3aa61a925b4eb0e2286cfb7e09bdc4ad88
Bander Al'Otebi <bander.alotebi@gmail.com>
2026-02-28 22:08:26 +0300

    “FIX_aichat”

--- dist/server/routes/chat.ts
+++ dist/server/routes/chat.ts
@@ -1,10 +1,6 @@
 import { Router } from "express";
 import * as fs from "fs";
 import * as path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 export const chatRouter = Router();
 
