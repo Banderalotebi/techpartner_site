a1f2a7b3adef1b670c77d3689118f5dccd11a545
Bander Al'Otebi <bander.alotebi@gmail.com>
2026-03-01 16:10:56 +0300

    Add missing dependencies (@google-cloud, pdfkit) and utility files

---
+++ server/prisma-client.ts
@@ -0,0 +1,9 @@
+// Prisma client stub - using Drizzle ORM instead
+// This file exists to satisfy imports from old code
+
+import { db } from "./db";
+
+export const prisma = db;
+
+export default prisma;
+
