17f16f21ba37d78e1dda6c17a9c46490dbc7a926
Bander Al'Otebi <bander.alotebi@gmail.com>
2026-03-02 05:06:23 +0300

    “FIX_EruIt”

--- dist/server/prisma-client.ts
+++
@@ -1,9 +0,0 @@
-// Prisma client stub - using Drizzle ORM instead
-// This file exists to satisfy imports from old code
-
-import { db } from "./db";
-
-export const prisma = db;
-
-export default prisma;
-
