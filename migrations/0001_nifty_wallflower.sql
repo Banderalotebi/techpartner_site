07b92348419acf82e8fbe2afefa0efcdd66072c6
Bander Al'Otebi <bander.alotebi@gmail.com>
2026-03-03 07:02:27 +0300

    Add migration for domainLeads table schema update

---
+++ migrations/0001_nifty_wallflower.sql
@@ -0,0 +1,5 @@
+ALTER TABLE "domain_leads" ADD COLUMN "company_name" text;--> statement-breakpoint
+ALTER TABLE "domain_leads" ADD COLUMN "email" text;--> statement-breakpoint
+ALTER TABLE "domain_leads" ADD COLUMN "phone" text;--> statement-breakpoint
+ALTER TABLE "domain_leads" ADD COLUMN "location" text;--> statement-breakpoint
+ALTER TABLE "domain_leads" ADD COLUMN "extracted_at" timestamp;
